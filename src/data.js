ccfdb3e857490dc9892a55bc930bc4db7d432da3
joycecoding <joyalonge01@gmail.com>
2021-08-17 11:56:21 -0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,225 @@
+// eslint-disable-next-line
+export default  {
+    products: [
+        {
+        _id:"1",
+        name: 'Women luxury gown',
+        category: 'gowns',
+        image: '/images/d1.jpg',
+        price: 6000,
+        brand: 'Fashion',
+        ratings: 4.5,
+        numReviews: 10
+    },
+        {
+        _id:"2",
+        name: 'Slim Shirt',
+        category: 'shirts',
+        image: '/images/d2.jpg',
+        price: 6000,
+        brand: 'Nike',
+        ratings: 4.5,
+        numReviews: 10
+    },
+        {
+         _id:"3",
+        name: 'Slim shirt for men',
+        category: 'pants',
+        image: '/images/d3.jpg',
+        price: 8000,
+        brand: 'Nike',
+        ratings: 4.5,
+        numReviews: 10
+    },
+        {
+            _id:"4",
+        name: 'Multicolored gown-Ladies',
+        category: 'gowns',
+        image: './images/d4.jpg',
+        price: 5000,
+        brand: 'Fashion',
+        ratings: 5,
+        numReviews: 10
+    },
+        {
+            _id:"5",
+        name: 'Ladies fashion wears',
+        category: 'shirts',
+        image: './images/d5.jpg',
+        price: 12000,
+        brand: 'Nike',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"6",
+        name: 'Women fashion heels',
+        category: 'gowns',
+        image: './images/d6.jpg',
+        price: 15000,
+        brand: 'Zara',
+        ratings: 5,
+        numReviews: 10
+    },
+        {
+            _id:"7",
+        name: 'Dior half-cap',
+        category: 'shirts',
+        image: './images/d11.jpg',
+        price: 2000,
+        brand: 'Dior',
+        ratings: 5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Women fashion heels',
+        category: 'shirts',
+        image: './images/d8.jpg',
+        price: 7500,
+        brand: 'Fashion',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Men-slim shirt',
+        category: 'shirts',
+        image: './images/d21.jpg',
+        price: 6000,
+        brand: 'Nike',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Gucci luxury wears',
+        category: 'wears',
+        image: './images/d10.jpg',
+        price: 9500,
+        brand: 'Gucci',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Dior half-acp',
+        category: 'shirts',
+        image: './images/d11.jpg',
+        price: 6000,
+        brand: 'Dior',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Best pants',
+        category: 'shirts',
+        image: './images/d12.jpg',
+        price: 6000,
+        brand: 'Fashion',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Zara slippers for ladies',
+        category: 'slippers',
+        image: './images/d13.jpg',
+        price: 5500,
+        brand: 'Zara',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Women fashion heels',
+        category: 'heels',
+        image: './images/d14.jpg',
+        price: 7500,
+        brand: 'EGO',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Men shirt -Roundneck for men',
+        category: 'shirts',
+        image: './images/d15.jpg',
+        price: 4500,
+        brand: 'Addidas',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Men shirt -Roundneck for men',
+        category: 'shirts',
+        image: './images/d16.jpg',
+        price: 5000,
+        brand: 'Gucci',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Men shirt -Roundneck for men',
+        category: 'shirts',
+        image: './images/d17.jpg',
+        price: 4500,
+        brand: 'Nike',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Women fashion heels',
+        category: 'Heels',
+        image: './images/d18.jpg',
+        price: 9000,
+        brand: 'Fashion',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Women fashion heels',
+        category: 'heels',
+        image: './images/d19.jpg',
+        price: 7500,
+        brand: 'Fashion',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+        name: 'Cute short jean-skirt',
+        category: 'skirts',
+        image: './images/d20.jpg',
+        price: 3000,
+        brand: 'Fashion',
+        ratings: 3.5,
+        numReviews: 10
+    },
+        {
+            _id:"8",
+            name: 'Zara slippers for ladies',
+            category: 'slippers',
+            image: './images/d13.jpg',
+            price: 5500,
+            brand: 'Zara',
+            ratings: 3.5,
+            numReviews: 10
+    },
+        {
+            _id:"8",
+            name: 'Women fashion heels',
+            category: 'Heels',
+            image: './images/d18.jpg',
+            price: 9000,
+            brand: 'Fashion',
+            ratings: 3.5,
+            numReviews: 10
+    },
+]
+}
